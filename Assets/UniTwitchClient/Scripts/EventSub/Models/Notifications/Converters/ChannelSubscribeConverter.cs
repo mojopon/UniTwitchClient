83aaa06b82fd1797234fedc9937f8cbc4b111606
Anomaloris <tyamuro@gmail.com>
2024-04-08 22:02:50 +0900

    ChannelSubscribeConverterとそのテストの実装

---
+++ Assets/UniTwitchClient/Scripts/EventSub/Models/Notifications/Converters/ChannelSubscribeConverter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniTwitchClient.EventSub.WebSocket;
+using UnityEngine;
+
+namespace UniTwitchClient.EventSub
+{
+    public class ChannelSubscribeConverter : INotificationConverter
+    {
+        public object Convert(Notification notification)
+        {
+            return new ChannelSubscribe(notification.UserId,
+                                        notification.UserName,
+                                        notification.UserLogin,
+                                        notification.BroadCasterUserId,
+                                        notification.BroadCasterUserName,
+                                        notification.BroadCasterUserLogin,
+                                        notification.Tier,
+                                        notification.IsGift
+                                        );
+        }
+    }
+}
--- Assets/UniTwitchClient/Tests/EventSub/Notification/NotificationConverterTest.cs
+++ Assets/UniTwitchClient/Tests/EventSub/Notification/NotificationConverterTest.cs
@@ -42,5 +42,37 @@ namespace UniTwitchClient.Tests.EventSub
             Assert.AreEqual(8, dateTime.Month);
             Assert.AreEqual(1, dateTime.Day);
         }
+
+        [Test]
+        public void ConvertChannelSubscribeTest() 
+        {
+            var notification = new Notification()
+            {
+                UserId = "1234",
+                UserLogin = "testUserLogin",
+                UserName = "testUserName",
+                BroadCasterUserId = "2345",
+                BroadCasterUserLogin = "broadcasterUserLogin",
+                BroadCasterUserName = "broadcasterUserName",
+                Tier = "1000",
+                IsGift = true,
+            };
+
+            var converter = new ChannelSubscribeConverter();
+            var result = converter.Convert(notification)as ChannelSubscribe;
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual("1234", result.UserId);
+            Assert.AreEqual("testUserLogin", result.UserLogin);
+            Assert.AreEqual("testUserName", result.UserName);
+
+            Assert.AreEqual("2345", result.BroadcasterUserId);
+            Assert.AreEqual("broadcasterUserLogin", result.BroadcasterUserLogin);
+            Assert.AreEqual("broadcasterUserName", result.BroadcasterUserName);
+
+            Assert.AreEqual("1000", result.Tier);
+            Assert.IsTrue(result.IsGift);
+        }
     }
 }
